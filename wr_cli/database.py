15d58e001d77f4ccdd52b94b30cccee2719dc7ac
Edmund Do <edo@uvm.edu>
2020-02-19 08:41:52 -0500

    Cleaned up database code (#20)

--- wr_cli/database.py
+++ wr_cli/database.py
@@ -1,3 +1,5 @@
+from resources import ResourceQuery as Q
+
 import csv
 import sqlite3
 
@@ -19,7 +21,7 @@ class Database:
         self.data_path = data_path
 
         self.connection = sqlite3.connect(db_path)
-        self.connection.row_factory = self.dict_factory
+        self.connection.row_factory = sqlite3.Row
         self.close = self.connection.close
 
         self.limit = limit
@@ -34,56 +36,43 @@ class Database:
             e.args = (*e.args, DatabaseError.MISSING_SCHEMA)
             raise e
 
+        # Read in data for each table from csv files
+        wine_array = self._build_query_args(f'{self.data_path}/Wine.csv')
+        review_array = self._build_query_args(f'{self.data_path}/Review.csv')
+        reviewer_array = self._build_query_args(f'{self.data_path}/Reviewer.csv')
+
+        # Insert data into wine, review and reviewer tables
+        self.connection.executemany(Q.INSERT_WINE, wine_array)
+        self.connection.executemany(Q.INSERT_REVIEW, review_array)
+        self.connection.executemany(Q.INSERT_REVIEWER, reviewer_array)
+
+        self.connection.commit()
+
+    def _build_query_args(self, path):
+        args = []
+
         try:
-            # Read in data for each table from csv files
-            wine_array = []
-            with open(f'{self.data_path}/Wine.csv') as wine_file:
-                csv_reader = csv.reader(wine_file)
+            with open(path) as f:
+                csv_reader = csv.reader(f)
                 next(csv_reader)
                 for row in csv_reader:
-                    wine_array.append(row)
-
-            review_array = []
-            with open(f'{self.data_path}/Review.csv') as review_file:
-                csv_reader = csv.reader(review_file)
-                next(csv_reader)
-                for row in csv_reader:
-                    review_array.append(row)
-
-            reviewer_array = []
-            with open(f'{self.data_path}/Reviewer.csv') as reviewer_file:
-                csv_reader = csv.reader(reviewer_file)
-                next(csv_reader)
-                for row in csv_reader:
-                    if row not in reviewer_array:
-                        reviewer_array.append(row)
+                    args.append(row)
         except FileNotFoundError as e:
             e.args = (*e.args, DatabaseError.MISSING_DATA)
             raise e
 
-        # Insert data into wine, review and reviewer tables
-        self.connection.executemany("INSERT INTO tblWine (country, price, province, variety, winery) "
-                                    "values (?, ?, ?, ?, ?)", wine_array)
-
-        self.connection.executemany(
-            "INSERT INTO tblReview (review_id, description, points, taster_twitter_handle, title, "
-            "variety, winery) values (?, ?, ?, ?, ?, ?, ?)", review_array)
-
-        self.connection.executemany("INSERT INTO tblReviewer (taster_name, taster_twitter_handle) "
-                                    "values (?, ?)", reviewer_array)
-
-        self.connection.commit()
+        return args
 
     def do_query(self, kw, **kwargs):
         try:
             if kw['_keyword'] == "wine":
-                return self.query_wine(kw, **kwargs)
+                return self._execute_query(kw, Q.SELECT_WINE, **kwargs)
 
             if kw['_keyword'] == "review":
-                return self.query_review(kw, **kwargs)
+                return self._execute_query(kw, Q.SELECT_REVIEW, **kwargs)
 
             if kw['_keyword'] == "reviewer":
-                return self.query_reviewer(kw)
+                return self._execute_query(kw, Q.SELECT_REVIEWER, **kwargs)
         except sqlite3.OperationalError as e:
             if 'table' in str(e):
                 raise ValueError(DatabaseError.NO_SUCH_TABLE)
@@ -91,63 +80,21 @@ class Database:
                 raise ValueError(DatabaseError.NO_SUCH_COLUMN)
             raise ValueError(DatabaseError.UNKNOWN_ERROR)
 
-    def dict_factory(self, cursor, row):
-        d = {}
-        for idx, col in enumerate(cursor.description):
-            d[col[0]] = row[idx]
-        return d
-
     def _get_limit_string(self, page_offset):
         offset = max(self.limit * (page_offset - 1), 0)
-        return f' LIMIT {self.limit} OFFSET {offset} '
+        return Q.ADDON_LIMIT.format(self.limit, offset)
 
-    def query_wine(self, kw, page_offset=0):
-        base_string = "SELECT * FROM tblWine"
-        add_on_string = self.add_on(kw)
+    def _execute_query(self, kw, query, page_offset=0):
+        add_on_string = self._add_on(kw)
         offset_string = self._get_limit_string(page_offset)
 
-        query = base_string + add_on_string + offset_string
-        wineSelect = self.connection.execute(query)
-        wineResults = wineSelect.fetchall()
+        query = query + add_on_string + offset_string
+        select = self.connection.execute(query)
+        results = select.fetchall()
 
-        return wineResults
+        return results
 
-    def query_review(self, kw, page_offset=0):
-        base_string = """
-            SELECT *
-            FROM
-                (
-                    SELECT review_id, description, points,
-                        tblReviewer.taster_name as taster_name, 
-                        tblReview.taster_twitter_handle as taster_twitter_handle, title,
-                        tblReview.variety as variety, tblWine.winery as winery,
-                        province, country, price
-                    FROM tblReview
-                    JOIN tblWine ON tblWine.variety = tblReview.variety AND tblReviewer.taster_twitter_handle = tblReview.taster_twitter_handle
-                    JOIN tblReviewer ON tblReviewer.taster_twitter_handle = tblReview.taster_twitter_handle
-                )
-            """
-        add_on_string = self.add_on(kw)
-        offset_string = self._get_limit_string(page_offset)
-
-        query = base_string + add_on_string + offset_string
-        reviewSelect = self.connection.execute(query)
-        reviewResults = reviewSelect.fetchall()
-
-        return reviewResults
-
-    def query_reviewer(self, kw, page_offset=0):
-        base_string = "SELECT * FROM tblReviewer"
-        add_on_string = self.add_on(kw)
-        offset_string = self._get_limit_string(page_offset)
-
-        query = base_string + add_on_string + offset_string
-        reviewerSelect = self.connection.execute(query)
-        reviewerResults = reviewerSelect.fetchall()
-
-        return reviewerResults
-
-    def add_on(self, kw):
+    def _add_on(self, kw):
         key_values = []
         for key, value in kw.items():
             if key == "_keyword":
---
+++ wr_cli/resources.py
@@ -0,0 +1,22 @@
+class ResourceQuery:
+    INSERT_WINE = """REPLACE INTO tblWine (country, price, province, variety, winery) values (?, ?, ?, ?, ?)"""
+    INSERT_REVIEW = """REPLACE INTO tblReview (review_id, description, points, taster_twitter_handle, title, variety, winery) values (?, ?, ?, ?, ?, ?, ?)"""
+    INSERT_REVIEWER = """REPLACE INTO tblReviewer (taster_name, taster_twitter_handle) values (?, ?)"""
+
+    SELECT_WINE = """Select * From tblWine"""
+    SELECT_REVIEWER = """SELECT * FROM tblReviewer"""
+    SELECT_REVIEW = """
+                    SELECT *
+                    FROM (
+                        SELECT review_id, description, points,
+                            tblReviewer.taster_name as taster_name, 
+                            tblReview.taster_twitter_handle as taster_twitter_handle, title,
+                            tblReview.variety as variety, tblWine.winery as winery,
+                            province, country, price
+                        FROM tblReview
+                        JOIN tblWine ON tblWine.variety = tblReview.variety AND tblReviewer.taster_twitter_handle = tblReview.taster_twitter_handle
+                        JOIN tblReviewer ON tblReviewer.taster_twitter_handle = tblReview.taster_twitter_handle
+                    )
+                    """
+
+    ADDON_LIMIT = """ LIMIT {0:d} OFFSET {1:d} """
